c9b9675e33cc818dfa6be41f06bcc9eacfed5e2e
Jacob Fox <jacobfox@jacobs-macbook-pro-2.local>
2017-08-24 14:52:36 -0700

    Initial Commit

---
+++ Font-Clock-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Font-Clock-2
+//
+//  Created by Jacob Fox on 2017-08-24.
+//  Copyright © 2017 Jacob Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
