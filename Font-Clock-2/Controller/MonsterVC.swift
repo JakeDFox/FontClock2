99ab0c6267599e293360b63dbd0fd027b7e3d24a
Jacob Fox <jacobfox@Jacobs-MacBook-Pro-2.local>
2017-11-06 00:01:21 -0800

    added monstervc and bodyartvc, working on more animations

---
+++ Font-Clock-2/Controller/MonsterVC.swift
@@ -0,0 +1,55 @@
+//
+//  MonsterVC.swift
+//  Font-Clock-2
+//
+//  Created by Jacob Fox on 2017-11-05.
+//  Copyright © 2017 Jacob Fox. All rights reserved.
+//
+
+import UIKit
+
+class MonsterVC: UIViewController {        
+        var timer: Timer!
+        var dateTimer: Timer!
+        var dateString: NSString!
+        @IBOutlet weak var timeFace: UILabel!
+        @IBOutlet weak var dateFace: UILabel!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            UIApplication.shared.isIdleTimerDisabled = true
+            setTimeFace()
+            setDateFace()
+            
+        }
+        
+        func setTimeFace() {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.setTimeFace), userInfo: nil, repeats: false)
+            let date: NSDate = NSDate()
+            let dateFormatter: DateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "h:mm a"
+            dateString = dateFormatter.string(from: date as Date) as NSString!
+            timeFace.text = dateString as String
+            
+        }
+        
+        
+        func setDateFace() {
+            dateTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.setDateFace), userInfo: nil, repeats: false)
+            let date: NSDate = NSDate()
+            let dateFormatter: DateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "EEE MMM dd yyyy"
+            dateString = dateFormatter.string(from: date as Date) as NSString!
+            dateFace.text = dateString as String
+        }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "toSettingsFromRocker" {
+                if let settingsVC = segue.destination as? SettingsVC {
+                    settingsVC.setDefault = "Set Rocker Clock as Default"
+                }
+            }
+        }
+}
+
+
---
+++ Font-Clock-2/View/NumbersToWords.swift
@@ -0,0 +1,11 @@
+//
+//  NumbersToWords.swift
+//  Font-Clock-2
+//
+//  Created by Jacob Fox on 2017-10-27.
+//  Copyright © 2017 Jacob Fox. All rights reserved.
+//
+
+import Foundation
+
+
